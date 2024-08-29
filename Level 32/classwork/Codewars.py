cd9f1a604a2d3686d3e8cb621497c53939197352
rostomi453 <rostomigo@gmail.com>
2024-08-29 22:47:05 +0400

    second commit

---
+++ Level 32/classwork/Codewars.py
@@ -0,0 +1,11 @@
+def is_isogram(string):
+    string = string.lower()
+    
+    for char in string:
+        if string.count(char) > 1:
+            return False
+    
+    return True
+
+
+print(is_isogram("luKA")) 
